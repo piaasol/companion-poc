c1e1a04504df8fe2630699fcc6e1efd2a77b6610
한미유미 <youmi@hanmiyumiui-MacBook-Pro.local>
2018-03-23 17:47:17 +0900

    run fixed

--- run.py
+++ run.py
@@ -23,17 +23,16 @@ logger = logging.getLogger(__name__)
 def run_mood(serve_forever=True):
     print("run_mood()")
     agent = Agent.load("models/dialogue",
-                       interpreter = RasaNLUInterpreter("models/nlu/default/current"))
+                       interpreter=RasaNLUInterpreter("models/nlu/default/current"))
     
     input_channel = SlackInput(slack_token="",  # this is the `bot_user_o_auth_access_token`
-                               slack_channel="general"  # the name of your channel to which the bot posts
-                               )
-                                                  
+                               slack_channel="bloomchatbot"  # the name of your channel to which the bot posts
+                               )                                      
     if serve_forever:
-                 print("input channel check==>",input_channel)
-                 agent.handle_channel(HttpInputChannel(5004, "/app", input_channel))
-                 
-                 agent.handle_channel(ConsoleInputChannel())
+         print("input channel check==>", input_channel)
+         agent.handle_channel(HttpInputChannel(5004, "/app", input_channel))
+         agent.handle_channel(ConsoleInputChannel())
+         return agent
 
 if __name__ == '__main__':
     utils.configure_colored_logging(loglevel="DEBUG")
